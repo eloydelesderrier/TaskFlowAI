28124cb660f16ae961fbfe9c333d697f32460c99
eloydelesderrier <eloy.delesderrier@hotmail.com>
2025-07-18 15:53:40 -0300

    Atualizações na rota

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import user, boards, lista, task, anexos
-from fastapi.responses import JSONResponse
-from fastapi.requests import Request
 
 app = FastAPI(
     title="TaskFlowAI",
@@ -12,17 +10,14 @@ app = FastAPI(
 
 
 
-@app.options("/{rest_of_path:path}")
-async def preflight_handler(request: Request, rest_of_path: str):
-    response = JSONResponse(content={"message": "Preflight OK"})
-    response.headers["Access-Control-Allow-Origin"] = request.headers.get("Origin", "*")
-    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
-    response.headers["Access-Control-Allow-Headers"] = request.headers.get("Access-Control-Request-Headers", "*")
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-    return response
 
-
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://tas-flow-frontend.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(user.router)
 app.include_router(boards.router)
