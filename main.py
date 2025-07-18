bba00fecd85921d966e809d172f1faee9dabeb11
eloydelesderrier <eloy.delesderrier@hotmail.com>
2025-07-18 14:08:08 -0300

    Atualizações na rota

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tas-flow-frontend.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
